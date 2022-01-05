dfcd452641982b16f7de3ecdbcdb7c3f4565cb49
Adam Hemzal <adahemzal@gmail.com>
2022-01-05 15:21:32 -0800

    added POST and GET request

--- server.js
+++ server.js
@@ -1,8 +1,57 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("<h1>hello aws</h1>");
+app.use(express.json());
+
+const catList = [
+  {
+    id: 1,
+    name: "Dimitrij",
+    breed: "Korat",
+    color: "dark grey",
+    age: 6,
+    sex: "Male",
+    image: "https://cataas.com/cat",
+  },
+  {
+    id: 2,
+    name: "Yuliya",
+    breed: "Ragdoll",
+    color: "white/brown",
+    age: 4,
+    sex: "Female",
+    image: "https://cataas.com/cat",
+  },
+  {
+    id: 3,
+    name: "Igor",
+    breed: "Ragamuffin",
+    color: "white/grey",
+    age: 5,
+    sex: "Male",
+    image: "https://cataas.com/cat",
+  },
+  {
+    id: 4,
+    name: "Katja",
+    breed: "Khao Manee",
+    color: "white",
+    age: 1,
+    sex: "Female",
+    image: "https://cataas.com/cat",
+  },
+];
+
+app.get("/api/cats", (req, res) => {
+  const singleCat = catList[Math.floor(Math.random() * catList.length)];
+  res.send(singleCat);
+});
+
+app.post("/api/cats", (req, res) => {
+  const data = req.body;
+  data.id = catList.length + 1;
+  catList.push(data);
+  res.send(data);
 });
 
 const port = process.env.PORT || 8080;
